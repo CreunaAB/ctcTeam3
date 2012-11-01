9ac1a173c97a96f728832d1fae9d6f9c8ddd991f
Jan Lofberg <chenebu@gmail.com>
2012-11-01 18:24:17 +0100

    up and running! yey

--- js/demo.js
+++ js/demo.js
@@ -1,32 +1,93 @@
 /*
  *  Declare standard objects for the API (model/view)
  */
-var sp = getSpotifyApi(1),
-    m = sp.require('sp://import/scripts/api/models'),
-    v = sp.require('sp://import/scripts/api/views'),
-    dom = sp.require('sp://import/scripts/dom');
+ var sp = getSpotifyApi(1),
+ m = sp.require('sp://import/scripts/api/models'),
+ v = sp.require('sp://import/scripts/api/views'),
+ dom = sp.require('sp://import/scripts/dom');
 
 
-$(document).ready(function() {
-  st.loadLatestTweet();
+ $(document).ready(function() {
+  st.loadLatestTweet(); // Init
 });
 
-var st = {
-  getTweet: function(data){
-    response = data[0].text;
-    $("#twitter-feed").html('<p>'+response+'</p>');
-  },
+ var st = {
   loadLatestTweet: function (){
-    var _url = 'https://api.twitter.com/1/statuses/user_timeline/chenebu.json?callback=?&count=1',
-        response;
+    var _url = 'https://api.twitter.com/1/statuses/user_timeline/chenebu.json?callback=?&count=1';
     $.getJSON(_url, st.getTweet);
   },
+
+  getTweet: function(data){
+    response = data[0].text;
+    var tracks = st.getSongsFromArray( st.splitString( response, ' ' ) );
+    console.log('tracks: ', tracks, 'length: ', tracks.length);
+    st.createPlaylist(tracks, response);
+  },
+
   splitString: function (string, split) {
     return string.split(split);
+  },
+
+  getSongsFromArray: function (words) {
+    var wordsongs = [],
+    i;
+
+    for ( i = 0; i < words.length; i++ ) {
+      wordsongs[i] = st.getSongFromWord(words[i]);
+    }
+
+    return wordsongs;
+  },
+
+  getSongFromWord: function (word) {
+    var songs = st.getSongsFromWord(word);
+    var song  = songs[ Math.floor( Math.random() * songs.length ) ];
+    return song;
+  },
+
+  getSongsFromWord: function(word) {
+    var search = new m.Search(word);
+
+    search.appendNext();
+    return search._tracks;
+
+  },
+
+  createPlaylist: function (trackList, tweet) {
+
+      // Create element for visual holder and set class (for css)
+      var playerHolder = $(document.createElement('div'));
+      playerHolder.addClass('player');
+      // Create title and enumeraion
+      var title = $(document.createElement('span'));
+
+      // Set class (for css)
+      title.addClass("title");
+      title.text(tweet);
+      // Add text to holder
+      playerHolder.append(title);
+      // Create spotify objects
+      var playlist = new m.Playlist();
+      var player = new v.Player();
+      // Fill up playlist
+      playlist.add(trackList);
+      // Set context
+      player.context = playlist;
+      // Set 1st track to player
+      player.track = playlist.get(0);
+      // Add it to dom
+      playerHolder.append(player.node);
+      $('.app').append(playerHolder);
+        // Visual list
+        var list = new v.List(playlist, function(track) {
+          return new v.Track(track, v.Track.FIELD.STAR | v.Track.FIELD.POPULARTIY | v.Track.FIELD.ARTIST | v.Track.FIELD.NAME | v.Track.FIELD.DURATION );
+        });
+      // Add list below player
+      playerHolder.append(list.node);
+
+    }
+
   }
-}
-
-
 
 
 
